7471b08e626a69927449325654d28a1a3773cd3a
Ben reeves <72570746+UP957029@users.noreply.github.com>
2025-10-28 10:54:04 +0000

    Program now also sends low price alerts to customer emails if a lower price is found

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ import os
 from dotenv import load_dotenv
 from requests.auth import HTTPBasicAuth
 from notification_manager import NotificationManager
+print("Program started.")
 load_dotenv()
 
 USERNAME = os.getenv('SHEETY_USERNAME')
@@ -16,19 +17,22 @@ PASSWORD = os.getenv("SHEETY_PASSWORD")
 SHEETY_TOKEN = os.environ.get('SHEETY_TOKEN')
 SHEETY_URL = "https://api.sheety.co/495bee945b60f16d5a876dcd6f631538/flightDeals/prices"
 
+print("After loading .env")
 res = requests.get(url=SHEETY_URL, auth=HTTPBasicAuth(USERNAME, PASSWORD))
 res.raise_for_status()
 data = res.json()
 sheet_data = data["prices"]
+print("After fetching Google Sheet data")
 
 flight_search = FlightSearch()
 data_manager = DataManager()
 notification_manager = NotificationManager()
+print("After creating NotificationManager")
 
 customer_data = data_manager.get_customer_emails()
 customer_emails = [user["whatIsYourEmail?"] for user in customer_data["users"]]
-print(customer_emails)
 
+print("Before flight search loop")
 for item in sheet_data:
     if item["iataCode"] == "":
         code = flight_search.get_iata_code(item["city"])
@@ -47,6 +51,8 @@ for item in sheet_data:
             print(flight.price)
             if float(flight.price) < float(lowest_price):
                 notification_manager.send_notification(flight.origin_airport, flight.destination_airport, flight.out_date, flight.return_date, flight.price)
+                if customer_emails:
+                    notification_manager.send_email(customer_emails,flight.origin_airport, flight.destination_airport, flight.out_date, flight.return_date, flight.price)
             else:
                 pass
         else:
--- notification_manager.py
+++ notification_manager.py
@@ -1,19 +1,23 @@
 import os
 from http import client
+import smtplib
 
 from twilio.rest import Client
 from dotenv import load_dotenv
 load_dotenv()
-
-print("SID:", os.getenv("TWILIO_ACCOUNT_SID"))
-print("Token:", os.getenv("TWILIO_AUTH_TOKEN"))
-
 
 class NotificationManager:
     def __init__(self):
         self.TWILIO_ACCOUNT_SID = os.getenv('TWILIO_ACCOUNT_SID')
         self.TWILIO_AUTH_TOKEN = os.getenv('TWILIO_AUTH_TOKEN')
         self.client = Client(self.TWILIO_ACCOUNT_SID, self.TWILIO_AUTH_TOKEN)
+
+        self.SMTP_USERNAME = os.getenv('EMAIL')
+        self.SMTP_PASSWORD = os.getenv('SMTP_PASSWORD')
+        self.connection = smtplib.SMTP('smtp.gmail.com', 587)
+        self.connection.starttls()
+        self.connection.login(self.SMTP_USERNAME, self.SMTP_PASSWORD)
+
 
     def send_notification(self, departure_airport, destination_airport, out_date, return_date, price):
         message = self.client.messages.create(
@@ -22,3 +26,12 @@ class NotificationManager:
             to="whatsapp:+447305974095"
         )
         print(f"Message sent: {message.status}")
+
+    def send_email(self, email_list, departure_airport, destination_airport, out_date, return_date, price):
+        for email in email_list:
+            self.connection.sendmail(
+                from_addr=self.SMTP_USERNAME,
+                to_addrs=email,
+                msg=f"Subject: Low Price Alert!\n\n-Low price alert! Only {price} to fly from {departure_airport} to {destination_airport} on {out_date} until {return_date}"
+            )
+
